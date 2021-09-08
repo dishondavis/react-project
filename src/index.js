ea1ec4de1d9b3758b187118e4333d36b7aff91f2
DishonDavis <shondavis33@gmail.com>
2021-09-08 16:07:53 -0400

    rendering a list of objects by using props and the spread operator. Added small styling change. mapping over an array of objects to receive and display the data.

--- src/index.js
+++ src/index.js
@@ -4,40 +4,44 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 // Setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81yXbSemHqL._AC_UL200_SR200,200_.jpg",
-  author: "Crystal Radke",
-  title: "My First Learn to Write Workbook",
-};
-
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91ttgU1OvWL._AC_UL200_SR200,200_.jpg",
-  author: "Qian Julie Wang",
-  title: "Beautiful Country: A Memoir",
-};
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81yXbSemHqL._AC_UL200_SR200,200_.jpg",
+    author: "Crystal Radke",
+    title: "My First Learn to Write Workbook",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91ttgU1OvWL._AC_UL200_SR200,200_.jpg",
+    author: "Qian Julie Wang",
+    title: "Beautiful Country: A Memoir",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/712%2BTA7g%2BYS._AC_UL200_SR200,200_.jpg",
+    author: "Verity",
+    title: "Colleen Hoover",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
