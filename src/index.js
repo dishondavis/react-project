0940f6ac52cadbd91b013f48f1bb0ab3ac89ea6a
DishonDavis <shondavis33@gmail.com>
2021-09-08 13:49:39 -0400

    created components for a booklist, added author, title, and book image with some css styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81yXbSemHqL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => (
+  <h1>
+    My First Learn to Write Workbook: Practice for Kids with Pen Control, Line
+    Tracing, Letters, and More! My First Learn to Write Workbook
+  </h1>
+);
+
+const Author = () => <h4>Crystal Radke</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
+// export default index
