7bd1ef2f2a3a17b53cb7c504ec039292b60450f8
DishonDavis <shondavis33@gmail.com>
2021-09-12 18:02:01 -0400

    created new files and seperated each component into its own file.

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81yXbSemHqL._AC_UL200_SR200,200_.jpg",
+    author: "Crystal Radke",
+    title: "My First Learn to Write Workbook",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91ttgU1OvWL._AC_UL200_SR200,200_.jpg",
+    author: "Qian Julie Wang",
+    title: "Beautiful Country: A Memoir",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/712%2BTA7g%2BYS._AC_UL200_SR200,200_.jpg",
+    author: "Verity",
+    title: "Colleen Hoover",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,48 +3,22 @@ import ReactDom from "react-dom";
 
 // CSS
 import "./index.css";
-// Setup vars
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81yXbSemHqL._AC_UL200_SR200,200_.jpg",
-    author: "Crystal Radke",
-    title: "My First Learn to Write Workbook",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91ttgU1OvWL._AC_UL200_SR200,200_.jpg",
-    author: "Qian Julie Wang",
-    title: "Beautiful Country: A Memoir",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/712%2BTA7g%2BYS._AC_UL200_SR200,200_.jpg",
-    author: "Verity",
-    title: "Colleen Hoover",
-  },
-];
+
+// JS
+import Book from "./Book";
+import { books } from "./books";
+import { greeting } from "./testing/testing";
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className="booklist">
-      {books.map((book) => {
+      {books.map((book, index) => {
         return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
-const Book = (props) => {
-  console.log(props);
-  const { img, title, author } = props;
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 // export default index
